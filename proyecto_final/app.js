be3b210f0318e7c62c6c99e13c74cebd5b214c6f
SpitfireMunguia <davidmunguiaamericas@gmail.com>
2017-11-05 15:28:15 -0600

    Cambios menores
    cambio en la estructura de los archivos

--- proyecto_final/app.js
+++ proyecto_final/app.js
@@ -4,15 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cons = require('consolidate');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+//Mongo connection
+
+
 var app = express();
 
 // view engine setup
+app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,7 +29,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
+//Listen
+app.listen(8000,function()
+{
+  console.log('Server is up');
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -40,7 +49,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error.jade');
 });
 
 module.exports = app;
